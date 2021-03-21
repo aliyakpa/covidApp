7fe6618f5c82405b8233845ab9d9bc860aab4f98
RuheeRajwani <ruhee.rajwani@gmail.com>
2021-03-21 15:55:27 -0400

    Finalized messages and formatting

--- routes/employee.js
+++ routes/employee.js
@@ -22,13 +22,13 @@ router.get('/', function (req, res, next) {
           return;
         } else {
           if (employee.EMPLOYEE_RETURN_STATUS_CD === "OE") {
-            res.render('message', { "message": "You are able to come in to the office! Please bring a mask and we will see you soon. ", "hasError": false });
+            res.render('message', { "message": "Yay! You are currently ABLE to come to the office! Please bring a mask and we will see you soon. ", "hasError": false });
             return;
           } else if (employee.EMPLOYEE_RETURN_STATUS_CD === "CP") {
-            res.render('message', { "message": "You are unable to come to the office at this time. Please follow all CDC guidelines and take care of yourself. We hope to see you soon!", "hasError": false });
+            res.render('message', { "message": "You are currently UNABLE to come to the office. Please follow all CDC guidelines and take care of yourself. We hope to see you soon!", "hasError": false });
             return;
           } else if (employee.EMPLOYEE_RETURN_STATUS_CD === "NTR") {
-            res.render('message', { "message": "Please take and submit a COVID test, so we can determine your return status.", "hasError": false });
+            res.render('message', { "message": "Please take and submit a COVID-19 test, so we can determine your return eligibility.", "hasError": false });
             return;
           }
 
